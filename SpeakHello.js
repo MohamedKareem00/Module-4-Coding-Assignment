85741c0c9ca016b31268437637042bf4ff5acfdd
Mohamed <MohamedKareem2006n@gmail.com>
2026-01-03 20:55:53 +0200

    module4-solution

---
+++ SpeakHello.js
@@ -0,0 +1,15 @@
+(function (window) {
+  // STEP 3
+  var helloSpeaker = {};
+
+  // DO NOT attach speakWord to the object
+  var speakWord = "Hello";
+
+  // STEP 4
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 5
+  window.helloSpeaker = helloSpeaker;
+})(window);
